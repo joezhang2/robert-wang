423db0b47fde4ca6246029247b738d8320a52a76
Rich Chang <rchang@signal.co>
2017-04-13 11:03:07 -0500

    try again

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var http, director, cool, bot, router, server, port, schedule;
+var botID = process.env.BOT_ID;
 
 http        = require('http');
 director    = require('director');
@@ -33,7 +34,40 @@ function ping() {
   this.res.end("Hey, I'm Cool Guy.");
 }
 
+function postMessage(message) {
+  var options, body, botReq;
+
+  options = {
+    hostname: 'api.groupme.com',
+    path: '/v3/bots/post',
+    method: 'POST'
+  };
+
+  body = {
+    "bot_id" : botID,
+    "text" : message
+  };
+
+  console.log('sending ' + message + ' to ' + botID);
+
+  botReq = HTTPS.request(options, function(res) {
+      if(res.statusCode == 202) {
+        //neat
+      } else {
+        console.log('rejecting bad status code ' + res.statusCode);
+      }
+  });
+
+  botReq.on('error', function(err) {
+    console.log('error posting message '  + JSON.stringify(err));
+  });
+  botReq.on('timeout', function(err) {
+    console.log('timeout posting message '  + JSON.stringify(err));
+  });
+  botReq.end(JSON.stringify(body));
+}
+
 //schedules
-schedule.scheduleJob('42 * * * *', function(){
-  bot.postMessage('testing');
+schedule.scheduleJob('10 * * * *', function(){
+  postMessage('testing');
 });
