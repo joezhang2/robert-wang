d8225cd106f4bea6431f7b54b76025af4f42c980
Joe Zhang <joe.zhang2@gmail.com>
2017-04-13 21:58:29 -0500

    displaying intentional commands in help

--- bot.js
+++ bot.js
@@ -97,7 +97,6 @@ function tryna_start() {
   }
 }
 
-<<<<<<< HEAD
 function eightball() {
   var options = ["It is known", " It is decidedly so", "Without a doubt", "Yes definitely", "You may rely on it", "As I see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy try again", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful", "I must say no"];
   options.push("You should ask " + choosePerson());
