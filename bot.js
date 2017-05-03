ad8d0d428a956e1ee9bc3201e4ca629e22443a51
Rich Chang <rchang@signal.co>
2017-05-03 16:54:49 -0500

    spam

--- bot.js
+++ bot.js
@@ -25,6 +25,14 @@ function respond() {
       this.res.writeHead(200);
       postMessage(choosePerson());
       this.res.end();
+    } else if (/^\/maximus$/.test(request_text)) {
+      this.res.writeHead(200);
+      postMessage(maximus());
+      this.res.end();
+    } else if (/^\/wtf$/.test(request_text)) {
+      this.res.writeHead(200);
+      postMessage(wtf());
+      this.res.end();
     } else if (/^\/8ball$/.test(request_text)) {
       this.res.writeHead(200);
       postMessage(eightball());
@@ -58,7 +66,7 @@ function cool_guy() {
 }
 
 function help() {
-  return "commands available are /cool guy and /choose";
+  return "commands available are /cool guy, /8ball and /choose";
 }
 
 function tryna_start() {
@@ -76,12 +84,19 @@ function eightball() {
   return options[index];
 }
 
+function wtf() {
+  return "What the fuck did you just fucking say about me, you little bitch? I’ll have you know I graduated top of my class in the Navy Seals, and I’ve been involved in numerous secret raids on Al-Quaeda, and I have over 300 confirmed kills. I am trained in gorilla warfare and I’m the top sniper in the entire US armed forces. You are nothing to me but just another target. I will wipe you the fuck out with precision the likes of which has never been seen before on this Earth, mark my fucking words. You think you can get away with saying that shit to me over the Internet? Think again, fucker. As we speak I am contacting my secret network of spies across the USA and your IP is being traced right now so you better prepare for the storm, maggot. The storm that wipes out the pathetic little thing you call your life. You’re fucking dead, kid. I can be anywhere, anytime, and I can kill you in over seven hundred ways, and that’s just with my bare hands. Not only am I extensively trained in unarmed combat, but I have access to the entire arsenal of the United States Marine Corps and I will use it to its full extent to wipe your miserable ass off the face of the continent, you little shit. If only you could have known what unholy retribution your little “clever” comment was about to bring down upon you, maybe you would have held your fucking tongue. But you couldn’t, you didn’t, and now you’re paying the price, you goddamn idiot. I will shit fury all over you and you will drown in it. You’re fucking dead, kiddo.";
+}
+
 function choosePerson() {
   var people = ["cory", "franklin", "peter", "kyle", "rich", "bernard", "michael", "kwang", "ept", "joe"];
   var index = Math.floor(Math.random() * people.length);
   return people[index];
 }
 
+function maximus() {
+  return "My name is Maximus Decimus Meridius, commander of the Armies of the North, General of the Felix Legions, loyal servant to the true emperor, Marcus Aurelius. Father to a murdered son, husband to a murdered wife. And I will have my vengeance, in this life or the next.";
+}
 function postMessage(message) {
   var options, body, botReq;
 
