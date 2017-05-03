35500fb51ce3cafcf76f6594e7f9ddc86fbcda08
Rich Chang <rchang@signal.co>
2017-05-03 16:41:25 -0500

    no

--- bot.js
+++ bot.js
@@ -70,7 +70,7 @@ function tryna_start() {
 }
 
 function eightball() {
-  var options = ["It is certain", " It is decidedly so", "Without a doubt", "Yes definitely", "You may rely on it", "As I see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy try again", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"];
+  var options = ["It is certain", " It is decidedly so", "Without a doubt", "Yes definitely", "You may rely on it", "As I see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy try again", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful", "I must say no"];
   options.push("You should ask " + choosePerson());
   var index = Math.floor(Math.random() * options.length);
   return options[index];
