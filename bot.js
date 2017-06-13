c13b1b33411c03a5058618bb81d12f4b2f2101df
Rich Chang <rchang@signal.co>
2017-06-13 15:31:46 -0500

    no empty mesages

--- bot.js
+++ bot.js
@@ -136,36 +136,38 @@ function jospeh(message, times) {
 }
 
 function postMessage(message) {
-  var options, body, botReq;
+  if (message != "") {
+    var options, body, botReq;
 
-  options = {
-    hostname: 'api.groupme.com',
-    path: '/v3/bots/post',
-    method: 'POST'
-  };
+    options = {
+      hostname: 'api.groupme.com',
+      path: '/v3/bots/post',
+      method: 'POST'
+    };
 
-  body = {
-    "bot_id" : botID,
-    "text" : jospeh(message, 15)
-  };
+    body = {
+      "bot_id" : botID,
+      "text" : jospeh(message, 15)
+    };
 
-  console.log('sending ' + message + ' to ' + botID);
+    console.log('sending ' + message + ' to ' + botID);
 
-  botReq = HTTPS.request(options, function(res) {
-      if(res.statusCode == 202) {
-        //neat
-      } else {
-        console.log('rejecting bad status code ' + res.statusCode);
-      }
-  });
+    botReq = HTTPS.request(options, function(res) {
+        if(res.statusCode == 202) {
+          //neat
+        } else {
+          console.log('rejecting bad status code ' + res.statusCode);
+        }
+    });
 
-  botReq.on('error', function(err) {
-    console.log('error posting message '  + JSON.stringify(err));
-  });
-  botReq.on('timeout', function(err) {
-    console.log('timeout posting message '  + JSON.stringify(err));
-  });
-  botReq.end(JSON.stringify(body));
+    botReq.on('error', function(err) {
+      console.log('error posting message '  + JSON.stringify(err));
+    });
+    botReq.on('timeout', function(err) {
+      console.log('timeout posting message '  + JSON.stringify(err));
+    });
+    botReq.end(JSON.stringify(body));
+  }
 }
 
 
